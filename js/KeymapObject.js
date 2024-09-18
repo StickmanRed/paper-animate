5df68c3d206c2ff7db7e8b35449e7fe1d914b26d
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-09-17 17:01:15 -0700

    Test JS workaround

--- js/KeymapObject.js
+++ js/KeymapObject.js
@@ -1,23 +1,30 @@
+// *sarcastically* Thanks, Javascript.
+// Okay, I guess this is how "this" is supposed to work, but it would be really nice to have some sort of auto-generating function function generator.
+function keyHandlerGenerator(thisValue) {
+    return function(event) {
+        /* @StickmanRed (note to self), check for Mac keyboards sometime
+        */
+        let keySequence = (event.ctrlKey ? "Control+" : "") 
+                        + (event.altKey ? "Alt+" : "")
+                        + (event.shiftKey ? "Shift+" : "")
+                        + String.fromCharCode(event.which);
+
+        if (Object.hasOwn(thisValue.keymap, keySequence)) {
+            window.globalProject.stackAdd(thisValue.keymap[keySequence]);
+        }
+    }
+}
+
 export class KeymapObject {
     constructor() {
         /* Key combinations are notated by keys separated by plus signs: "Shift+e"
          * I don't think anyone will need shortcuts like "q+a"?
          */
         this.keymap = {};
+        this.keyHandler = keyHandlerGenerator(this);
         this.$attached = null; // Meaning attached element
     }
-    keyHandler(event) {
-        /* @StickmanRed (note to self), check for Mac keyboards sometime
-         */
-        let keySequence = (event.ctrlKey ? "Control+" : "") 
-                        + (event.altKey ? "Alt+" : "")
-                        + (event.shiftKey ? "Shift+" : "")
-                        + String.fromCharCode(event.which);
 
-        if (Object.hasOwn(this.keymap, keySequence)) {
-            window.globalProject.stackAdd(this.keymap[keySequence]);
-        }
-    }
     assign(keys) {
         Object.assign(this.keymap, keys);
     }
