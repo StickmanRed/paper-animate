6a34cf652e27f349dc17961c0bba3bacc49985fd
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-10-02 14:08:22 -0700

    Fix previous test test fixes to test fixes

--- js/windowInterface/Window.js
+++ js/windowInterface/Window.js
@@ -16,7 +16,7 @@ export class Window {
         .css("touch-action", "none");
 
         this.$resizeContainer = $(`<div class="window-resizers" id="Window-Resizers${Window.WINDOW_ID}"></div>`).appendTo(this.$element);
-        /* this.$resizers = $()
+        this.$resizers = $()
                          .add($(`<div class="window-resizeTop" id="Window-ResizeTop${Window.WINDOW_ID}"></div>`).css("height", Window.RESIZE_DISTANCE))
                          .add($(`<div class="window-resizeBottom" id="Window-ResizeBottom${Window.WINDOW_ID}"></div>`).css("height", Window.RESIZE_DISTANCE))
                          .add($(`<div class="window-resizeLeft" id="Window-ResizeLeft${Window.WINDOW_ID}"></div>`).css("width", Window.RESIZE_DISTANCE))
@@ -51,14 +51,13 @@ export class Window {
                     });
                 }
             }
-        }); */
+        });
 
         this.$move = $(`<div class="window-draggable" id="Window-Move${Window.WINDOW_ID}"></div>`).css({
             flex: `0 0 ${Window.HEADER_HEIGHT}px`
         }).appendTo(this.$element);
 
         // This should work however "Window-Move" is set up.
-        // @StickmanRed (note to self): This doesn't work. Investigate!
         let startPos, deltaPos;
         this.interactMove = interact(this.$move[0]).draggable({
             listeners: {
